90f1f9acfc9354aa678fb18e9f5a9f03f41547ff
saurabhnikam1997 <nikam280197@gmail.com>
2020-10-12 08:58:48 +0530

    modify for singlet combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,10 +1,39 @@
 #!/bin/bash -x
 
-echo "Welcome to Flip coin Simulation"
+declare -A singletDict
+singletDict=(["Heads"]=0 ["Tails"]=0)
+headCount=0
+tailCount=0
+noOfFlips=10
 
-if [[ $((RANDOM%2)) -eq 1 ]]
-then
-   echo Heads
-else
-   echo Tails
-fi
+function headOrTail()
+{
+	if [[ $((RANDOM%2)) -eq 1 ]]
+	then
+		echo Heads
+		coinOutput="Heads"
+	else
+		echo Tails
+		coinOutput="Tails"
+	fi
+}
+
+function singletCombination()
+{
+	for (( i=0; i<$noOfFlips; i++ ))
+	do
+		headOrTail
+		if [[ $coinOutput == Heads ]]
+		then
+			singletDict[Heads]=$((${singletDict[Heads]}+1 ))
+			((headCount++))
+		else
+			singletDict[Tails]=$((${singletDict[Tails]}+1 ))
+			((tailCount++))
+		fi
+	done
+	headPercentage=`expr "scale=2; $headCount * 100 / $noOfFlips" | bc`
+	tailPercentage=`expr "scale=2; $tailCount * 100 / $noOfFlips" | bc`
+}
+singletCombination
+
